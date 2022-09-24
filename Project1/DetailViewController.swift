4e194f25e3291d8d9f23cf99d5ab00cc5fe78133
zug <dungnguyen.ttcb@gmail.com>
2022-09-24 22:35:27 +0700

    Adding rightBarButtonView for sharing image

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -20,6 +20,8 @@ class DetailViewController: UIViewController {
         title = "Picture " + String(selectedIndex + 1) + " of " + String(imageCount)
         navigationItem.largeTitleDisplayMode = .never
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         // Do any additional setup after loading the view.
         if let nameImg = selectedImage {
             imageView.image = UIImage(named: nameImg)
@@ -37,14 +39,15 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func shareTapped() {
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
-    */
 
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 I'm on my way to becoming an iOS developer.
 
-# Project1
-An example on Hacking with Swift: https://www.hackingwithswift.com/read/1/1/setting-up
+# Project3
+An example on Hacking with Swift: https://www.hackingwithswift.com/read/3/overview
